35f91c1e2f91d59be47d41586cab7fea22ea49be
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-04-09 10:57:51 -0400

    changed path.join to resolve

--- index.js
+++ index.js
@@ -2,43 +2,43 @@
 //   require("dotenv").config();
 // }
 
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const port = process.env.PORT;
-const path = require("path");
-const ejsMate = require("ejs-mate");
-const userRoutes = require("./routes/user");
-const showRoutes = require("./routes/shows");
-const songRoutes = require("./routes/songs");
-const venueRoutes = require("./routes/venues");
-const helmet = require("helmet");
-const auth = require("./auth.js");
-const view = require("./view.js");
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const userRoutes = require('./routes/user');
+const showRoutes = require('./routes/shows');
+const songRoutes = require('./routes/songs');
+const venueRoutes = require('./routes/venues');
+const helmet = require('helmet');
+const auth = require('./auth.js');
+const view = require('./view.js');
 
-app.engine("ejs", ejsMate);
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
 
 const scriptSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://cdn.jsdelivr.net/npm/fuse.js/dist/fuse.js",
+  'https://api.mapbox.com/',
+  'https://cdn.jsdelivr.net/npm/fuse.js/dist/fuse.js',
 ];
 
 const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://events.mapbox.com/",
+  'https://api.mapbox.com/',
+  'https://events.mapbox.com/',
 ];
 
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      "script-src": ["'self'", "'unsafe-inline'", ...scriptSrcUrls],
-      "worker-src": ["blob:"],
-      "connect-src": ["'self'", ...connectSrcUrls],
+      'script-src': ["'self'", "'unsafe-inline'", ...scriptSrcUrls],
+      'worker-src': ['blob:'],
+      'connect-src': ["'self'", ...connectSrcUrls],
     },
   })
 );
@@ -53,12 +53,12 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/", userRoutes);
-app.use("/", showRoutes);
-app.use("/", songRoutes);
-app.use("/", venueRoutes);
+app.use('/', userRoutes);
+app.use('/', showRoutes);
+app.use('/', songRoutes);
+app.use('/', venueRoutes);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   class ExpressError extends Error {
     constructor(message, statusCode) {
       super();
@@ -66,7 +66,7 @@ app.all("*", (req, res, next) => {
       this.statusCode = statusCode;
     }
   }
-  next(new ExpressError("Page Not Found", 404));
+  next(new ExpressError('Page Not Found', 404));
 });
 
 // catch 404 and forward to error handler
@@ -75,13 +75,13 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render("simple-message", view.errorMessage(req, err));
+  res.render('simple-message', view.errorMessage(req, err));
 });
 
 app.listen(port, () => {
