0e61db391b0c37915fb30eec1619a14018620b05
andrewaltman1 <55675406+andrewaltman1@users.noreply.github.com>
2023-04-12 13:08:18 -0400

    cleaned up ignore files

--- public/scripts/confirmation.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    window.addEventListener("DOMContentLoaded", () => {
-      document.querySelector("#view-mode-button").classList.toggle("hide");
-      document
-        .querySelector(".table-mode-container")
-        .classList.toggle("center-children");
-      document.querySelector("h3").style.display = "none";
-    });
-  })();
